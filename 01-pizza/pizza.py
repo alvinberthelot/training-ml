ba02a8856a778102410782bc661c5376038bf34e
Alvin Berthelot <alvin.berthelot@webyousoon.com>
2018-01-19 10:27:01 +0100

    Introduce problem with nice print

--- 01-pizza/pizza.py
+++ 01-pizza/pizza.py
@@ -4,4 +4,7 @@ import pandas as pandas
 
 input_file = "pizzas.csv"
 pizzas = pandas.read_csv(input_file, header = 0)
-print(pizzas.keys())
+
+print("Start linear regression with pizzas\n")
+print("Example of data with the first 3 lines:")
+print(pizzas[:3])
