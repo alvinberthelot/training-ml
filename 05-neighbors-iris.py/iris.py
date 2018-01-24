b40daa915a3ea6c01dbd9cc6c9c469074add0c25
Alvin Berthelot <alvin.berthelot@webyousoon.com>
2018-01-24 14:51:07 +0100

    Add model neighbors with training 05 about iris

--- 05-neighbors-iris.py/iris.py
+++ 05-neighbors-iris.py/iris.py
@@ -12,6 +12,15 @@ y = iris.target
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-plt.scatter(X_train[:, 0], X_train[:, 1], c=y_train)
+# plt.scatter(X_train[:, 0], X_train[:, 1], c=y_train)
 # plt.scatter(X_train[:, 2], X_train[:, 1], c=y_train)
-plt.show()
+# plt.show()
+
+model = neighbors.KNeighborsClassifier(n_neighbors=10)
+model.fit(X_train, y_train)
+
+predictions = model.predict(X_test)
+
+print("Accuracy score :", metrics.accuracy_score(y_test, predictions))
+print("Classification report :\n", metrics.classification_report(y_test, predictions))
+print("Confusion matrix :\n", metrics.confusion_matrix(y_test, predictions))
